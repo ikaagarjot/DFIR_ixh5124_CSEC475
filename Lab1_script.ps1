fd4d47153c2b52a2ae3efbb9b1b82d40e6b988c4
ikaagarjot <ihothi@gmail.com>
2017-09-08 00:53:17 -0400

    Created and added script to repo
    This is a *mostly* working script. Features still need to be fleshed
    out, but the deadline is approaching so this is what I'm turning in

---
+++ Lab1_script.ps1
@@ -0,0 +1,32 @@
+﻿# Lab 1: Artifacts Script
+# This script grabs many artifacts from a machine including
+# Time, Uptime, Operating System Info, Network Info, and Software
+
+# This script is missing features due to them not being corrected
+# prior to the deadline
+
+"Timezone: " + (Get-TimeZone)
+$date = (Get-Date)
+"Date/Time: " + $date
+$uptime = Get-WmiObject -Class win32_operatingsystem
+$uptime = $uptime.ConvertToDateTime($uptime.LastBootUpTime)
+"Uptime: " + (new-timespan -start $uptime -end $date)
+[System.Environment]::OSVersion.Version
+"`n"
+"OS Version: " + ((Get-WmiObject -class Win32_OperatingSystem).Caption)
+"CPU Brand: " + (Get-WmiObject win32_processor).Manufacturer
+"CPU Type: " + (Get-WmiObject win32_processor).Name
+$mem = (Get-WmiObject -Class Win32_ComputerSystem).TotalPhysicalMemory
+$mem = [Math]::Round($mem/1024MB)
+"RAM: " + $mem + "GB"
+Get-PSDrive -PSProvider FileSystem
+"Hostname: " + (Get-WmiObject -Class Win32_ComputerSystem -Property Name).Name
+Get-WmiObject -Class Win32_UserAccount | Format-Table
+Get-Service |select -property name,starttype | Format-Table
+Get-ScheduledTask
+arp -a
+ipconfig /all
+netstat -a
+Get-WmiObject -Class Win32_Product | Format-Table
+Get-Process
+#Export-Csv C:\scriptresult.txt
